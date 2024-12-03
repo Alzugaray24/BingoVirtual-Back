65b9483cfda4444170e93b450b3b32f9bb2a9286
Matias <alzugaray1997@gmail.com>
2024-12-03 17:18:15 -0300

    cambio

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/", (req, res) => {
-  res.send("Servidor funcionando");
+  res.send("Servidor");
 });
 
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando");
+  res.send("Servidor del sexo");
 });
 
 export default app;
